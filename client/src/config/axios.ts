0069db41a3bd3469cad485ae6b2e3090c3f3b9cd
vanhuyhvh2k4 <vanhuyhvh2004@gmail.com>
2025-10-24 17:47:31 +0700

    chore(axios)/get server base url from env file

--- client/src/config/axios.ts
+++ client/src/config/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: import.meta.env.VITE_SERVER_BASE_URL,
   headers: {
     'Content-Type': 'application/json'
   }
